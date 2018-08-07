7d5d5ec6c5f3071feed911e7da3bd2c093edf223
Paulo Vitor Sato <psato@ciandt.com>
2018-08-07 10:24:35 -0300

    add header and default context for background

--- devcamp2018/app/src/main/java/com/psato/devcamp/interactor/usecase/UseCase.kt
+++ devcamp2018/app/src/main/java/com/psato/devcamp/interactor/usecase/UseCase.kt
@@ -1,8 +1,36 @@
+/**
+ * (C) Copyright 2018 Paulo Vitor Sato for Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.psato.devcamp.interactor.usecase
 
 import android.util.Log
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
+import kotlin.coroutines.experimental.CoroutineContext
+
+
+/**
+ * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
+ * This interface represents a execution unit for different use cases (this means any use case
+ * in the application should implement this contract).
+ * <p>
+ * By convention each UseCase implementation will return the result using a coroutine
+ * that will execute its job in a background thread and will post the result in the UI thread.
+ */
 
 abstract class UseCase<T> {
 
@@ -28,8 +56,8 @@ abstract class UseCase<T> {
         }
     }
 
-    protected suspend fun <X> background(block: suspend () -> X): Deferred<X> {
-        return async(CommonPool, parent = parentJob) {
+    protected suspend fun <X> background(context: CoroutineContext = CommonPool, block: suspend () -> X): Deferred<X> {
+        return async(context, parent = parentJob) {
             block.invoke()
         }
     }
