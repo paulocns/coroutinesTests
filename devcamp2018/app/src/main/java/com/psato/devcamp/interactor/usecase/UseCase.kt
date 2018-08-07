b585eafa051f1c881e5d9cdf9e39f4e410c56d93
Paulo Vitor Sato <psato@ciandt.com>
2018-08-07 10:31:23 -0300

    update header

--- devcamp2018/app/src/main/java/com/psato/devcamp/interactor/usecase/UseCase.kt
+++ devcamp2018/app/src/main/java/com/psato/devcamp/interactor/usecase/UseCase.kt
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2018 Paulo Vitor Sato for Open Source Project
+ * (C) Copyright 2018 Paulo Vitor Sato Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
